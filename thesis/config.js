58414c720057b84d8c5c998f264fb5116f45bb4f
XinyiTu <116761432+XinyiTu@users.noreply.github.com>
2023-08-10 15:17:54 -0400

    update

--- thesis/config.js
+++ thesis/config.js
@@ -3,7 +3,7 @@ let opening_text =
 
 let text_1 = 
 '<div><p>After the notice was served on his business, 96-year-old Thomas W. Munkenbeck hustled around town to seek a new location for his pipe and ship fitting shop. He had worked in his shop for 74 years–until one of the air shafts for the highway tunnel was set to be put right on the site.</p>' +
-'<p>“He suddenly became ill,” said his son Earl Mukenbeck, who ran the business with elder Munkenbeck, according to the Brooklyn Daily Eagle. The son showed up at Brooklyn Supreme Court in place of his father. The court granted them until December to leave the premise.</p></div>'
+'<p>“He suddenly became ill,” said his son Earl Mukenbeck, who ran the business with elder Munkenbeck, according to the Brooklyn Daily Eagle. The son showed up at Brooklyn Supreme Court in place of his father. The court granted them until December to leave the premises.</p></div>'
 
 let text_2 = ''
 
@@ -13,20 +13,20 @@ let text_4 = '<div><p>Between 2nd Avenue and 3rd Avenue, the entire block north 
 
 let text_5 ='<div><img id="video" src="media/banner-vid.gif"style="filter: drop-shadow(0 0 0.1rem black);"><figcaption>MTA Bridges & Tunnels Special Archive</figcaption><p>Along Hamilton Avenue, about 1,000 residents were evicted, and some 400 dwellings were razed to make way for the incoming highway.</p></div>'
 
-let text_6 = '<p>Among the displaced were about 100 welfare tenants, police officer, baker, bar and grill owner, local metal works shop that served the waterfront for decades, and a beloved local restaurant known as the “Roast Beef King.”</p><img src="media/sanborn-map.png"/><figcaption>1939 Sanborn Fire Insurance Map. Source: Library of Congress<br>Visualization: Cynthia Tu</figcaption><p>Hamilton Avenue was the artery of the buzzing neighborhood of Red Hook. Since the Dutch colonists arrived at the waterfront area in South Brooklyn in the 1600s, Red Hook had been a vibrant immigrant enclave. A diverse mix of ethnic groups: Dutch, Norwegians, Italians, Puerto Ricans, and Blacks (both enslaved people brought over by the Dutch, and free blacks who fled the South)… have called Red Hook their home.</p>'+
+let text_6 = '<p>Among the displaced were about 100 welfare tenants, police officers, bakers, bar and grill owners, local metal works shop that served the waterfront for decades, and a beloved local restaurant known as the “Roast Beef King.”</p><img src="media/sanborn-map.png"/><figcaption style="margin-left: 18vw">1939 Sanborn Fire Insurance Map. Source: Library of Congress<br>Visualization: Cynthia Tu</figcaption><p>Hamilton Avenue was the artery of the buzzing neighborhood of Red Hook. Since the Dutch colonists arrived at the waterfront area in South Brooklyn in the 1600s, Red Hook had been a vibrant immigrant enclave. A diverse mix of ethnic groups: Dutch, Norwegians, Italians, Puerto Ricans, and Blacks (both enslaved people brought over by the Dutch, and free Blacks who fled the South) have called Red Hook their home.</p>'+
 '<p>From the 1840s into the 20th Century, Red Hook bustled with fishermen, dockworkers, sailors, and shop owners who visited, worked, or lived on the waterfront.</p>'+ 
 "<p>“Think of it as a maritime Times Square,” wrote Carolina Salguero, founder of Red Hook Waterfront Stories.</p><p>Yet, Red Hook soon lost its glory due to the city’s planning policies. In the eyes of the federal government, Red Hook was one of the least desirable neighborhoods in New York City.</p><br>"+
 '<h3>A RED LINE, FROM THE MAP TO THE GROUND</h3>'+
-'<p>When the Great Depression hit in 1929, foreclosures swept across cities in America. In hopes of reinforcing better real estate practices, the federal government established the Home Owners Loan Corporation (HOLC), an agency that appraises neighborhoods across the country. In the 1930s, HOLC created a series of maps that assess lending risk in different neighborhoods. Neighborhoods were classified into four grades: "<span style="background-color: #85d364">best</span>," "<span style="background-color: #79b2e6">desirable</span>,” “<span style="background-color:#e6b22d">declining</span>,” and “<span style="background-color: #dd5f5f">hazardous</span>.”</p>'+
+'<p>When the Great Depression hit in 1929, foreclosures swept across cities in America. In hopes of reinforcing better real estate practices, the federal government established the Home Owners Loan Corporation (HOLC), an agency that appraised neighborhoods across the country. In the 1930s, HOLC created a series of maps that assess lending risk in different neighborhoods. Neighborhoods were classified into four grades: "<span style="background-color: #85d364">best</span>," "<span style="background-color: #79b2e6">desirable</span>,” “<span style="background-color:#e6b22d">declining</span>,” and “<span style="background-color: #dd5f5f">hazardous</span>.”</p>'+
 '<p>The HOLC report of Red Hook reads:</p>'+'<p id="holc">Population: <strong>35% foreign-born families, predominately Irish-Italian-Scandinavian.</strong><br>Occupation: <strong>unskilled labor</strong><br><br>Detrimental Influences: <strong>...Mixed races. "Red Hook" and "Gowanus Canal" sections have been slum-type areas for many years...</strong><br>Favorable Influences: <span style="color:brown"><strong>Proposed Hamilton Avenue tunnel to the Battery in Manhattan.</strong></span></p>'
 
 let text_7='<div><p>Red Hook was marked as “hazardous” and thus <span style="background-color: #dd5f5f">colored in red</span>.</p></div>'
 
 let text_8 ='<p>These documents, now known as the “redlining” maps, would dictate America’s urban planning policies, resulting in economic inequality and residential segregation.</p>'+
 
-"<p>“It's crystal clear the role that race–and on a highly lesser extent, ethnicity plays in it,” said Dr. Rober Nelson, director of America Panorama: An Atlas of United States History at the University of Richmond. “You did have this hierarchy of ethnic populations, and they’re not all treated the same.” <p/>" +
+"<p>“It's crystal clear the role that race,and on a highly lesser extent, ethnicity plays in it,” said Dr. Rober Nelson, director of America Panorama: An Atlas of United States History at the University of Richmond. “You did have this hierarchy of ethnic populations, and they’re not all treated the same.” <p/>" +
 
-"<p>Take European immigrant communities as an example, Nelson explained that HOLC tends to give higher grades to immigrant communities coming from Western and Protestant Europe, while Eastern and Southern Europe or Catholic countries tend to receive lower grades. Black neighborhoods were also redlined regardless of their socioeconomic status.</p>"+
+"<p>Take European immigrant communities as an example. Nelson explained that HOLC tended to give higher grades to immigrant communities coming from Western and Protestant Europe, while Eastern and Southern Europe or Catholic countries tend to receive lower grades. Black neighborhoods were also redlined regardless of their socioeconomic status.</p>"+
 
 "<p>“We think about redlining as a form of divestment,” Nelson said. “What we see is that divestment in residential housing, and in all kinds of infrastructure improves the quality of life, but they often see a fair amount of investment in things like highways, interstates, and infrastructure that is often compromising the quality of life.”</p>"
 
@@ -112,10 +112,10 @@ var config = {
             image: 'media/munkenbeck.png',
             chapterDiv: text_1,
             location: {
-                center: [-74.005921, 40.683500],
-                zoom: 20,
-                pitch: 90,
-                bearing: 85,
+                center: [-74.006232, 40.683692],
+                zoom: 21.39,
+                pitch: 77.21,
+                bearing: 33.01,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
